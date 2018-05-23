60207a4f906f45c65e346d1b02c44c49e8f78c4b
Artur <artsspe@gmail.com>
2018-05-23 23:37:57 +0200

    Add test file for tests purpose.

---
+++ tests.cpp
@@ -0,0 +1,20 @@
+#define BOOST_TEST_MODULE module_test
+#include <boost/test/included/unit_test.hpp>
+#include <QApplication>
+#include <iostream>
+#include "Server.hpp"
+
+namespace utf = boost::unit_test;
+
+
+BOOST_AUTO_TEST_CASE( connection_vector )
+{
+  Server server;
+
+}
+
+
+
+
+
+
